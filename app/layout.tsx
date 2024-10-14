7a4e37080d391c4e56fce209c64c90271bdf28e0
944750720 <944750720@qq.com>
2024-10-15 00:22:22 +0900

    projects done, scroll and click header motion done

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,7 @@
 import Header from "@/components/header";
 import './globals.css'
 import { Inter } from 'next/font/google'
-// import { ThemeContextProvider } from '@/context/theme-context';
-// import { ActiveSectionContextProvider } from '@/context/active-section-context';
+import ActiveSectionContextProvider from '@/context/active-section-context';
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -16,12 +15,14 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className='!scroll-smooth'>
       <body className={`${inter.className} bg-gray-50 text-gray-950 relative h-[5000px] pt-28 sm:pt-36`}>
         <div className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[0rem] h-[31.25rem] w-[31.25rem] rounded-full sm:w-[68.75rem] blur-[10rem]"></div>
         <div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full sm:w-[55rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:-left-[-5rem] blur-[10rem]"></div>
-        <Header />
-        {children}
+        <ActiveSectionContextProvider>
+          <Header />
+          {children}
+        </ActiveSectionContextProvider>
       </body>
     </html>
   )
