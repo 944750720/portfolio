7bd1b360e5273c35c8aaabe9bad210968a6a2608
944750720 <944750720@qq.com>
2024-10-18 15:47:27 +0900

    optimization for phone

--- components/project.tsx
+++ components/project.tsx
@@ -26,7 +26,7 @@ export default function Project({ title, description, tags, imageUrl, href }: Pr
         >
             <section
                 className='group bg-gray-100 max-w-[42rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:h-[34rem] group-even:pl-8 hover:bg-gray-200 transition mb-3 sm:mb-8 last:mb-0'>
-                <div className='pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full group-even:ml-[21rem]'>
+                <div className='pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:group-odd:mr-0 sm:group-odd:ml-0 sm:max-w-[50%] flex flex-col h-full group-even:ml-[21rem] sm:group-even:ml-[21rem] group-even:mr-[0rem] group-odd:mr-[18rem] group-odd:ml-[7rem]'>
                     <h3 className='text-2xl font-semibold'>{title}</h3>
                     <p className='mt-2 leading-relaxed text-gray-700'>{description}</p>
                     <ul className='flex flex-wrap mt-4 gap-2 sm:mt-auto'>
@@ -37,7 +37,7 @@ export default function Project({ title, description, tags, imageUrl, href }: Pr
                 </div>
 
                 <a href={href} target="_blank" className="block !cursor-pointer">
-                    <Image src={imageUrl} alt="Project I worked on" quality={95} className='absolute top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl
+                    <Image src={imageUrl} alt="Project I worked on" quality={95} className='absolute top-8 -right-44 w-[28.25rem] rounded-t-lg shadow-2xl
         group-hover:scale-[1.04]
         group-hover:-translate-x-3
         group-hover:translate-y-3
